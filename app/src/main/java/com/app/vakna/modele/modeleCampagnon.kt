b7e39c3c9dc70f2d66732b22e0b63de33a19b54b
Sofian <sofiangho31000@outlook.fr>
2024-10-14 15:35:14 +0200

    modeleCompagnon modif

--- app/src/main/java/com/app/vakna/modele/modeleCampagnon.kt
+++ app/src/main/java/com/app/vakna/modele/modeleCampagnon.kt
@@ -2,19 +2,35 @@ package com.app.vakna.modele
 
 class modeleCampagnon(
     nom:String,
-    private val faim:Int = 50,
-    private val humeur:Int = 50,
-    private val xp:Int = 0,
+    private var faim:Int = 50,
+    private var humeur:Int = 50,
+    private var xp:Int = 0,
     private val espece:String
 ){
     fun modifierFaim(niveau:Int){
-        faim = niveau + faim
-        assert(faim > 100) {"Le niveau de faim ne peut pas depasser de 100"}
-        if(faim > 100)
-            faim = 100
+        if(niveau > 0)
+            faim += niveau
+            assert(faim > 100) {"Le niveau de faim ne peut pas depasser de 100"}
+            if(faim > 100)
+                faim = 100
+        else
+            faim -= niveau
+            assert(faim > 0){"Le niveau de faim ne peut pas descendre en dessous 0"}
+            if(faim < 0)
+                faim = 0
 
     }
     fun modifierHumeur(niveau: Int){
+        if(niveau > 0)
+            humeur += niveau
+        assert(humeur > 100) {"Le niveau de faim ne peut pas depasser de 100"}
+        if(humeur > 100)
+            humeur = 100
+        else
+            humeur -= niveau
+        assert(humeur > 0){"Le niveau de faim ne peut pas descendre en dessous 0"}
+        if(humeur < 0)
+            humeur = 0
 
     }
     fun gagnerXp(montant:Int){
