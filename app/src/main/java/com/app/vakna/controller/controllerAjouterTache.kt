1a635906e2e308e4c05991f56a8a25641f3626c5
Sofian <sofiangho31000@outlook.fr>
2024-10-15 11:35:37 +0200

    controllerAjouterTache schéma controller

--- app/src/main/java/com/app/vakna/controller/controllerAjouterTache.kt
+++ app/src/main/java/com/app/vakna/controller/controllerAjouterTache.kt
@@ -1,13 +1,33 @@
 package com.app.vakna.controller
 import android.widget.EditText
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Spinner
 import com.app.vakna.R
 
 @Controller
 class controllerCreationTache(private val view: View){
-    fun recupererDonneeTache(){
+    fun recupererNomTache(){
         val nomTacheEditText = view.findViewById<EditText>(R.id.editTextNomTache)
         val nomTache = nomTacheEditText.text.toString()
+    }
+    fun recupererTypeTache(){
+        val type = view.findViewById<Spinner>(R.id.spinnerCategory)
+        val typeDeLaTache = spinnerCategory.selectedItem.toString()
+    }
+    fun recupererFrequenceTache(){
+        val radioGroupPriority = view.findViewById<RadioGroup>(R.id.radioGroupPriority)
+        val selectedRadioButtonId = radioGroupPriority.checkedRadioButtonId
+
+        when(selectedRadioButtonId){
+
+        }
+
+        /*if (selectedRadioButtonId != -1) {
+            val selectedRadioButton = view.findViewById<RadioButton>(selectedRadioButtonId)
+            val selectedPriority = selectedRadioButton.text.toString()
+        }*/
     }
 }
 
