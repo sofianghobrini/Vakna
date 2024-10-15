8bd4b8bddeb14eb0ab90049cc09d1e45f79337f6
Sofian <sofiangho31000@outlook.fr>
2024-10-15 15:21:38 +0200

    modifier test

--- app/src/main/java/com/app/vakna/controller/controllerAjouterTache.kt
+++ app/src/main/java/com/app/vakna/controller/controllerAjouterTache.kt
@@ -15,19 +15,31 @@ class controllerCreationTache(private val view: View){
     fun recupererTypeTache(){
         val type = view.findViewById<Spinner>(R.id.spinnerCategory)
         val typeDeLaTache = spinnerCategory.selectedItem.toString()
+        when(typeDeLaTache){
+            "Urgent"->{}
+            "Sport"->{}
+            "Travail"->{}
+
+
+        }
     }
     fun recupererFrequenceTache(){
-        val radioGroupPriority = view.findViewById<RadioGroup>(R.id.radioGroupPriority)
+        val radioGroupPriority = view.findViewById<RadioGroup>(R.id.radioFrequenceTache)
         val selectedRadioButtonId = radioGroupPriority.checkedRadioButtonId
 
         when(selectedRadioButtonId){
+            R.id.radioQuotidien -> {
+                val frequenceView = view.findViewById<RadioGroup>(R.id.radioQuotidien)
+                val frequence = frequenceView.selectedItem
+            }
+            R.id.radioHebdomadaire -> {
 
+            }
+            R.id.radioMensuel -> {
+
+            }
+            else->{}
         }
-
-        /*if (selectedRadioButtonId != -1) {
-            val selectedRadioButton = view.findViewById<RadioButton>(selectedRadioButtonId)
-            val selectedPriority = selectedRadioButton.text.toString()
-        }*/
     }
 }
 
