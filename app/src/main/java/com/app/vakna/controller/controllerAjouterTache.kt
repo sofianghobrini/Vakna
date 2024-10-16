9ef7d34e02de21f5b203df0b79a37cf7c92dbab4
Sofian <sofiangho31000@outlook.fr>
2024-10-16 10:01:14 +0200

    amelioration controllerAjouterTache

--- app/src/main/java/com/app/vakna/controller/controllerAjouterTache.kt
+++ app/src/main/java/com/app/vakna/controller/controllerAjouterTache.kt
@@ -1,35 +1,66 @@
-package com.app.vakna.controller
+/*import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
-import android.view.View
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Spinner
 import com.app.vakna.R
 
 @Controller
-class controllerCreationTache(private val view: View){
+class controllerTache(private val view: View){
     fun recupererNomTache(){
         val nomTacheEditText = view.findViewById<EditText>(R.id.editTextNomTache)
         val nomTache = nomTacheEditText.text.toString()
     }
-    fun recupererTypeTache(){
-        val type = view.findViewById<Spinner>(R.id.spinnerCategory)
-        val typeDeLaTache = spinnerCategory.selectedItem.toString()
-        when(typeDeLaTache){
+    fun recupererTypeTache():String?{
+        val type = view.findViewById<Spinner>(R.id.selectTypeTache)
+        val typeDeLaTache = type.selectedItem.toString()
+        return when(typeDeLaTache){
+            "Urgent"->{
+                "Urgent"
+            }
+            "Sport"->{
+                "Sport"
+            }
+            "Travail"->{
+                "Travail"
+            }
+            else->"Non défini"
 
         }
     }
-    fun recupererFrequenceTache(){
-        val radioGroupPriority = view.findViewById<RadioGroup>(R.id.radioFrequenceTache)
-        val selectedRadioButtonId = radioGroupPriority.checkedRadioButtonId
+    fun recupererFrequenceTache(): String? {
+        val radioGroupFrequence = view.findViewById<RadioGroup>(R.id.radioFrequenceTache)
+        val selectedRadioButtonId = radioGroupFrequence.checkedRadioButtonId
 
-        when(selectedRadioButtonId){
-            R.id.radioQuotidien -> {}
-            R.id.radioHebdomadaire -> {}
-            R.id.radioMensuel -> {}
-            else->{}
+        return when (selectedRadioButtonId) {
+            R.id.radioQuotidien -> {
+                "Quotidien"
+            }
+            R.id.radioHebdomadaire -> {
+                "Hebdomadaire"
+            }
+            R.id.radioMensuel -> {
+                "Mensuel"
+            }
+            else -> {
+                null // Aucun bouton sélectionné
+            }
         }
+    }
+    fun recupererImportanceTache():String{
+        val IdImportance  = view.findViewById<CheckBox>(R.id.checkboxImportance)
+        if(IdImportance.isChecked){
+            return "Important"
+        }
+        else{
+            return "Pas important"
+        }
+    }
+    fun EnvoyerInformationTache(){
+
     }
 }
 
 annotation class Controller
+*/
