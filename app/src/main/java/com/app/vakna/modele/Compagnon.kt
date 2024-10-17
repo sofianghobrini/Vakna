2d830609a65eaeafdfc1372be414307ef945a79e
Beurisseuh <brice.monkonkala@etu.iut-tlse3.fr>
2024-10-17 09:58:24 +0200

    - Rajouté les classes CompagnonDAO et Compagnon depuis la branche BackEnd

--- app/src/main/java/com/app/vakna/modele/Compagnon.kt
+++ app/src/main/java/com/app/vakna/modele/Compagnon.kt
@@ -1,23 +1,15 @@
 package com.app.vakna.modele
 
+import com.app.vakna.modele.dao.CompagnonDAO
+
 class Compagnon(
-    nom:String,
-    private var faim:Int = 50,
-    private var humeur:Int = 50,
-    private var xp:Int = 0,
-    private val espece:String
+    var nom:String,
+    var faim:Int = 50,
+    var humeur:Int = 50,
+    var xp:Int = 0,
+    val espece:String
 ){
-    fun getFaim(): Int {
-        return faim
-    }
-
-    fun getHumeur(): Int {
-        return humeur
-    }
-
-    fun getXp(): Int {
-        return xp
-    }
+    private val dao = CompagnonDAO()
 
     fun modifierFaim(niveau:Int){
         assert(niveau in -100..100) { "Le niveau de faim doit être compris entre -100 et 100." }
@@ -27,6 +19,7 @@ class Compagnon(
             faim = 100
         else if(faim <0)
             faim = 0
+        dao.modifier(nom, this)
     }
     fun modifierHumeur(niveau: Int){
         assert(niveau in -100..100) { "Le niveau d'humeur doit être compris entre -100 et 100." }
@@ -36,12 +29,15 @@ class Compagnon(
             humeur = 100
         else if(humeur <0)
             humeur = 0
+        dao.modifier(nom, this)
     }
+
     fun gagnerXp(montant:Int){
         val ancienNiveau = niveau()
         xp += montant
         if (ancienNiveau > niveau())
             xp -= montant
+        dao.modifier(nom, this)
     }
 
     fun niveau(): Int {
@@ -52,5 +48,9 @@ class Compagnon(
         if (xp < 1000)
             return 2
         return 3
+    }
+
+    override fun toString(): String {
+        return "$nom ($espece) : Faim = $faim, Humeur = $humeur, XP = $xp (niveau ${niveau()})"
     }
 }
