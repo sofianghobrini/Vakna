5eee6b5dd2a8859f4d12f874e6a529665cc8b00e
Benjamin FOUILHAC <bfouilhac@gmail.com>
2024-10-21 12:44:18 +0200

    - Ajout d'un getter pour le gestionnaire de compagnons dans GestionnaireDeTaches dans Tache.kt - Mise en commentaire de la méthode ajouterTaches(taches: List<Tache>) dans Tache.kt - Suppression d'import useless dans ModeleTacheTest.kt - Fix des créations d'instances de gestionnaire et de setCompagnon dans ModeleTacheTest.kt - Fix des tests pour check les bonus/malus sur l'humeur et l'XP dans ModeleTacheTest.kt - Ajout d'un test unitaire pour check si les bonus sont effectifs quand une tâche est finie dans ModeleTacheTest.kt -

--- app/src/main/java/com/app/vakna/modele/Tache.kt
+++ app/src/main/java/com/app/vakna/modele/Tache.kt
@@ -8,7 +8,7 @@ import com.app.vakna.modele.dao.TacheDAO
 import java.time.LocalDate
 
 // Enums pour la tâche
-enum class Frequence { QUOTIDIENNE, HEBDOMADAIRE, MENSUELLE}
+enum class Frequence { QUOTIDIENNE, HEBDOMADAIRE, MENSUELLE }
 enum class Importance { FAIBLE, MOYENNE, ELEVEE }
 enum class TypeTache { PERSONNELLE, PROFESSIONNELLE, PROJET, ETUDES, SPORT, VIEQUO, AUTRE }
 
@@ -17,7 +17,7 @@ class Tache(
     var frequence: Frequence,
     var importance: Importance,
     var type: TypeTache,
-    var derniereValidation: LocalDate,
+    var derniereValidation: LocalDate? = null,
     var estTerminee: Boolean = false,
     var estArchivee: Boolean = false
 ) {
@@ -25,7 +25,7 @@ class Tache(
         return ListData(nom, type.name, importance.name, 0, estTerminee, estArchivee)
     }
 
-    override fun equals(other: Any?): Boolean{
+    override fun equals(other: Any?): Boolean {
         if (other !is Tache) return false
         return nom == other.nom
     }
@@ -35,11 +35,11 @@ class Tache(
     }
 
     override fun toString(): String {
-        return "$nom : $frequence $importance $type $derniereValidation " + if(estTerminee)"Finie" else "Pas Finie" + if(estArchivee)" ARCHIVEE" else ""
+        return "$nom : $frequence $importance $type $derniereValidation " + if (estTerminee) "Finie" else "Pas Finie" + if (estArchivee) " ARCHIVEE" else ""
     }
 }
 
-class GestionnaireDeTaches(private var context : Context) {
+class GestionnaireDeTaches(private var context: Context) {
     private var tacheDAO = TacheDAO(context)
     private val setDeTaches = mutableSetOf<Tache>()
     private var gestionnaireCompagnons = GestionnaireDeCompagnons(CompagnonDAO(context))
@@ -53,6 +53,10 @@ class GestionnaireDeTaches(private var context : Context) {
         this.idCompagnon = id
     }
 
+    fun getGestionnaireCompagnon(): GestionnaireDeCompagnons{
+        return gestionnaireCompagnons
+    }
+
     fun ajouterTache(tache: Tache): Boolean {
         if (tache.nom.isBlank()) {
             throw IllegalArgumentException("Le nom de la tâche ne peut pas être vide")
@@ -63,19 +67,20 @@ class GestionnaireDeTaches(private var context : Context) {
         return tacheDAO.inserer(tache)
     }
 
+    /** Fonction inutile pour le moment
     fun ajouterTaches(taches: List<Tache>): Boolean {
-        var toutesInsertionsOK = true
-        if (taches.isEmpty()) {
-            throw IllegalArgumentException("La liste de tâches à ajouter ne peut pas être vide")
-        }
-        for (t in taches) {
-            ajouterTache(t)
-            if (!tacheDAO.inserer(t)) {
-                toutesInsertionsOK = false
-            }
-        }
-        return toutesInsertionsOK
+    var toutesInsertionsOK = true
+    if (taches.isEmpty()) {
+    throw IllegalArgumentException("La liste de tâches à ajouter ne peut pas être vide")
     }
+    for (t in taches) {
+    ajouterTache(t)
+    if (!tacheDAO.inserer(t)) {
+    toutesInsertionsOK = false
+    }
+    }
+    return toutesInsertionsOK
+    }*/
 
     fun modifierTache(nom: String, nouvelleTache: Tache): Boolean {
         val tache = setDeTaches.find { it.nom == nom }
@@ -99,7 +104,10 @@ class GestionnaireDeTaches(private var context : Context) {
             if (!tache.estTerminee) {
                 tache.estTerminee = true
                 tache.derniereValidation = LocalDate.now()
-                gestionnaireCompagnons.modifierHumeur(idCompagnon, -5 * (tache.importance.ordinal + 1))
+                gestionnaireCompagnons.modifierHumeur(
+                    idCompagnon,
+                    -5 * (tache.importance.ordinal + 1)
+                )
                 gestionnaireCompagnons.gagnerXp(idCompagnon, -5 * (tache.importance.ordinal + 1))
             }
             setDeTaches.remove(tache)
@@ -115,17 +123,16 @@ class GestionnaireDeTaches(private var context : Context) {
         if (tache != null) {
             tache.estTerminee = true
             tache.derniereValidation = LocalDate.now()
-            gestionnaireCompagnons.modifierHumeur(idCompagnon, 10 * (tache.importance.ordinal + 1))
+            gestionnaireCompagnons.modifierHumeur(idCompagnon, 5 * (tache.importance.ordinal + 1))
             gestionnaireCompagnons.gagnerXp(idCompagnon, 5 * (tache.importance.ordinal + 1))
-            Log.i("test2", gestionnaireCompagnons.obtenirCompagnons().find { it.id == 1 }!!.toString())
         } else {
-            throw IllegalArgumentException("Tâche avec le nom $nom introuvable $setDeTaches")
+            throw IllegalArgumentException("Tâche avec le nom $nom introuvable")
         }
         tacheDAO.modifier(nom, tache)
     }
 
     fun archiverTache(nom: String): Boolean {
-        val tacheAArchiver = setDeTaches.find { it.nom == nom }?:return false
+        val tacheAArchiver = setDeTaches.find { it.nom == nom } ?: return false
         tacheAArchiver.estArchivee = true
         tacheDAO.modifier(nom, tacheAArchiver)
         return true
@@ -148,6 +155,7 @@ class GestionnaireDeTaches(private var context : Context) {
     fun obtenirTaches(frequence: Frequence): Set<Tache> {
         return obtenirTaches().filter { it.frequence == frequence }.toSet()
     }
+
     fun obtenirTachesParFrequence(): Map<Frequence, Set<Tache>> {
         return obtenirTaches().groupBy { it.frequence }.mapValues { it.value.toSet() }
     }
@@ -163,7 +171,7 @@ class GestionnaireDeTaches(private var context : Context) {
     companion object {
         fun setToListDataArray(taches: Set<Tache>): ArrayList<ListData> {
             val list = ArrayList<ListData>()
-            for( tache in taches) {
+            for (tache in taches) {
                 val listData = tache.toListData()
                 list.add(listData)
             }
