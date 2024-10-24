cf225e3ff6af21a66acbdd319816eca2ade3b7bd
Pierre-Louis <pierrelouischaux@gmail.com>
2024-10-24 15:56:22 +0200

    test pour voir si  c'est le intent quii marxhe pas normalement ça doit ammener vers gere les tache

--- app/src/main/java/com/app/vakna/ProjetsActivity.kt
+++ app/src/main/java/com/app/vakna/ProjetsActivity.kt
@@ -2,22 +2,28 @@ package com.app.vakna
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import com.app.vakna.controller.ControllerAjouterTache
+import com.app.vakna.controller.ControllerGerer
 import com.app.vakna.databinding.ActivityAjouterBinding
+import com.app.vakna.databinding.ActivityGererBinding
 import com.app.vakna.modele.dao.TacheDAO
 
 class ProjetsActivity: AppCompatActivity() {
-    private lateinit var binding: ActivityAjouterBinding
+    private lateinit var binding: ActivityGererBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityAjouterBinding.inflate(layoutInflater)
+        // Inflate the layout using View Binding
+        binding = ActivityGererBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val root: View = binding.root
 
-
+        ControllerGerer(binding)
     }
+
 }
