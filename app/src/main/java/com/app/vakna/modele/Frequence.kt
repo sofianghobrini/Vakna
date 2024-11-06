c2d703c1d92a68ceffec0f6e53e828cbc14aa0a3
Benjamin FOUILHAC <bfouilhac@gmail.com>
2024-11-06 13:09:57 +0100

    - Revert du refacto pour le pousser sur une autre branche

--- app/src/main/java/com/app/vakna/modele/Frequence.kt
+++
@@ -1,7 +0,0 @@
-package com.app.vakna.modele
-
-sealed class Frequence {
-    data object QUOTIDIENNE : Frequence()
-    data class HEBDOMADAIRE(val jours: Set<JourSemaine>) : Frequence()
-    data class MENSUELLE(val joursDuMois: Set<Int>) : Frequence()
-}
