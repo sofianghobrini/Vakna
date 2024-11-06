1032edfc03719bbf2902b67cb9721ed6ae175b2d
Benjamin FOUILHAC <bfouilhac@gmail.com>
2024-11-06 11:29:12 +0100

    - Fix warning à la con

--- app/src/main/java/com/app/vakna/modele/Frequence.kt
+++ app/src/main/java/com/app/vakna/modele/Frequence.kt
@@ -1,7 +1,7 @@
 package com.app.vakna.modele
 
 sealed class Frequence {
-    object Quotidienne : Frequence()
+    data object Quotidienne : Frequence()
     data class Hebdomadaire(val jours: Set<JourSemaine>) : Frequence()
     data class Mensuelle(val joursDuMois: Set<Int>) : Frequence()
 }
