09c90c958303d760ede52a8123825c1f68f6905a
sofianghobrini <sofiangho31000@outlook.fr>
2024-12-16 13:13:51 +0100

    ajout de personnalite

--- app/src/main/java/com/app/vakna/modele/Enums.kt
+++ app/src/main/java/com/app/vakna/modele/Enums.kt
@@ -5,4 +5,15 @@ enum class Frequence { QUOTIDIENNE, HEBDOMADAIRE, MENSUELLE }
 enum class Importance { FAIBLE, MOYENNE, ELEVEE }
 enum class TypeTache { PERSONNELLE, PROFESSIONNELLE, PROJET, ETUDES, SPORT, VIEQUO, AUTRE }
 enum class TypeObjet { JOUET, NOURRITURE }
-enum class Personnalite {GOURMAND, JOUEUR, CALME, CUPIDE, AVARD, GRINCHEUX}
+enum class Personnalite(val facteurFaim: Float, val facteurHumeur: Float, val facteurPrix : Float, val facteurXp : Float ) {
+    GOURMAND(0.75f, 1.25f,1.0f,1.0f),
+    JOUEUR(1.0f, 1.25f,1.0f,1.0f),
+    CALME(1.0f, 1.0f,1.0f,1.0f),
+    CUPIDE(1.0f, 0.9f,1.0f,1.0f),
+    AVARE(1.0f, 1.25f,1.0f,1.0f),
+    GRINCHEUX(1.0f, 1.25f,1.0f,1.0f),
+    RADIN(1.0f,1.0f, 1.25f,1.0f),
+    GENTIL(1.0f,1.0f,0.75f, 1.0f),
+    JOYEUX(1.0f,0.75f,1.0f,1.0f),
+    RACISTE(2.0f,2.0f,2.0f,2.0f)
+}
